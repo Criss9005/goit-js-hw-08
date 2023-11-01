bdcf723b5f36b0042321bdbdabeffe0dec42fcb1
Criss9005 <cdjpn5@gmail.com>
2023-11-01 13:46:33 -0500

    video ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"; 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach(element => {
+    const item = document.createElement('li');
+    gallery.append(item);
+    item.classList.add('gallery__item')
+    const link = document.createElement('a');
+    item.append(link);
+    link.classList.add('gallery__link');
+    
+    link.href = element.original;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    link.append(image);
+    image.src = element.preview
+    image.alt = element.description
+    image.data = element.original
+
+});
+
+
+
+gallery.addEventListener('click', (e) => { 
+    e.preventDefault()
+    let galleryShow = new SimpleLightbox('.gallery li a'); 
+    
+})
